7d4963e59825f5262582aa74f6f8816e88020f14
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-09-07 21:46:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 function Person(name, age) {
     this.name = name;
     this.age = age;
-    this.greet = function() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
-    };
 }
 
-const person = Object.create(Person.prototype);
-person.name = 'Alice';
-person.age = 14;
-person.greet();
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
 function Employee(name, age, jobTitle) {
-    Person.call(this, name, age);
+    Person.call(this, name, age); 
     this.jobTitle = jobTitle;
-    this.introduce = function() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
-    };
 }
 
 Employee.prototype = Object.create(Person.prototype);
+
 Employee.prototype.constructor = Employee;
 
-const employee = Object.create(Employee.prototype);
-employee.name = 'Bob';
-employee.age = 30;
-employee.jobTitle = 'Developer';
-employee.introduce();
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+
+const person = new Person("Alice", 25);
+person.greet(); 
+
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet(); 
 
 window.Person = Person;
 window.Employee = Employee;
